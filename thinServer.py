db05903682c3b97a176689d4dfbc37ed7349935a
Sören Giller <sorengiller@Sorens-MacBook-Air.local>
2017-07-20 20:08:13 +0200

    server startet als thread

--- thinServer.py
+++ thinServer.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from flask import *
+import socket, select, string, sys, time, json
+from threading import Thread
 
 app = Flask(__name__)
 
+def start():
+    host = '0.0.0.0'
+    port = 50001
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((host, port))
+    s.listen(3)
+    while True:
+        c, addr = s.accept()
+        print("Connection from : " + str(addr))
 
 @app.route('/')
 def hello_world():
@@ -9,4 +20,7 @@ def hello_world():
 
 
 if __name__ == '__main__':
+    tserver = Thread(target=start)
+    tserver.daemon = True
+    tserver.start()
     app.run()
